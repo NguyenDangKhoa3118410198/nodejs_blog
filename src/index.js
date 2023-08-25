b9d543542399a25c1e3dbb2169a464c8565bd348
Nguyen Dang Khoa <nguyendangkhoa.3118410198@gmail.com>
2023-08-25 16:19:38 +0700

    feat: sort desc, asc  /me/store/courses

--- src/index.js
+++ src/index.js
@@ -1,36 +1,60 @@
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const exphbs = require('express-handlebars');
-const methodOverride = require('method-override');
+const express = require('express')
+const path = require('path')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-const route = require('./routes');
-const db = require('../src/config/db');
+const SortMiddleware = require('./app/middlewares/SortMiddleware')
 
-db.connect();
-app.use(methodOverride('_method'));
+const route = require('./routes')
+const db = require('../src/config/db')
+
+db.connect()
+app.use(methodOverride('_method'))
 
 // app.use(morgan("combined"));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(SortMiddleware)
 
 app.engine(
     'hbs',
     exphbs.engine({
         defaultLayout: 'main',
         extname: '.hbs',
-        helpers: { sum: (a, b) => a + b },
-    })
-);
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources', 'views'));
+        helpers: {
+            sum: (a, b) => a + b,
+            sortable: (field, sort) => {
+                const sortType = field === sort.column ? sort.type : 'default'
+                const icons = {
+                    default: 'chevron-expand-outline',
+                    asc: 'caret-up-outline',
+                    desc: 'caret-down-outline',
+                }
 
-route(app);
+                const types = {
+                    default: 'desc',
+                    asc: 'desc',
+                    desc: 'asc',
+                }
+
+                const icon = icons[sortType]
+                const type = types[sortType]
+
+                return `<a href="?_sort&column=${field}&type=${type}"> <ion-icon name="${icon}"></ion-icon> </a>`
+            },
+        },
+    })
+)
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources', 'views'))
+
+route(app)
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-});
+    console.log(`App listening on port ${port}`)
+})
